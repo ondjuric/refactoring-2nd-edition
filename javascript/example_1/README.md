900d1f85fc0b8bdfb4f22e4ad26ab95cc76f7dfb
Sageful Path <ondjuric@users.noreply.github.com>
2024-07-14 23:11:26 +0200

    Change parent dir

---
+++ javascript/example_1/README.md
@@ -0,0 +1,1 @@
+# Refactoring 2nd edtion
---
+++ javascript/example_1/createStatementData.js
@@ -0,0 +1,97 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount() {
+        let result = 0;
+        switch (this.play.type) {
+            case "tragedy":
+                throw 'bad thing';
+            case "comedy":
+                throw new Error('subclass responsibility');
+            default:
+                throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return result;
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        return result;
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+
+        return result;
+    }
+}
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+
+    }
+}
+
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`);
+    }
+}
+
+/**************************************
+ * 
+ * CREATE STATEMENT
+ * 
+ */
+
+export default function createStatementData(invoice, plays) {
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    return result;
+
+    function enrichPerformance(aPerformance) {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+        const result = Object.assign({}, aPerformance);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function totalAmount(data) {
+        return data.performances.reduce((result, perf) => result += perf.amount, 0);
+    }
+
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((result, perf) => result += perf.volumeCredits, 0);
+    }
+}
